0b07c78b33780742e250e9d1246d33ee3f58a64c
Chiff001 <74724374+Chiff001@users.noreply.github.com>
2024-11-24 21:26:17 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from "react-redux";
+import store from "./store";
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App></App>
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
