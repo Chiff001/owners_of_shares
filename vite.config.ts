c7623a0239dbb84bfe8d9a975bbb5a2b6eefe38c
Chiff001 <74724374+Chiff001@users.noreply.github.com>
2024-11-25 22:24:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
-  server: { 
+  plugins: [react()],
+  base: "/RIP_node",
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  server: {
+    // Tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: '0.0.0.0',
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://172.23.192.1:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/"),
       },
     }
-   },
-  plugins: [react()],
-})
+  },
+  // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
+});
