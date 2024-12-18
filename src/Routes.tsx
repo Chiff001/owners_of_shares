155c0e21f154cb6e4da4912fe3c50db2d887b402
Chiff001 <74724374+Chiff001@users.noreply.github.com>
2024-12-18 15:36:43 +0300

    Delete src/Routes.tsx

--- src/Routes.tsx
+++
@@ -1,9 +0,0 @@
-export const ROUTES = {
-  HOME: "/",
-  PERSONALITIES: "/personalities",
-}
-export type RouteKeyType = keyof typeof ROUTES;
-export const ROUTE_LABELS: {[key in RouteKeyType]: string} = {
-  HOME: "Компания",
-  PERSONALITIES: "Лица",
-};
