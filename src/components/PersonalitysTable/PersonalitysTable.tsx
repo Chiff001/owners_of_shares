06a0890f5e97b9a72070b65c3a26b42f2a13acf6
Chiff001 <74724374+Chiff001@users.noreply.github.com>
2024-12-18 15:48:52 +0300

    Create PersonalitysTable.tsx

---
+++ src/components/PersonalitysTable/PersonalitysTable.tsx
@@ -0,0 +1,76 @@
+import {useNavigate} from "react-router-dom";
+import {useMemo} from "react";
+import {Button} from "reactstrap";
+import {T_Personality} from "modules/types.ts";
+import CustomTable from "components/CustomTable/CustomTable.tsx";
+import {deletePersonality} from "store/slices/personalitysSlice.ts";
+import {useAppDispatch} from "store/store.ts";
+
+type Props = {
+    personalitys:T_Personality[]
+}
+
+const PersonalitysTable = ({personalitys}:Props) => {
+
+    const navigate = useNavigate()
+
+    const dispatch = useAppDispatch()
+
+    const handleClick = (personality_id) => {
+        navigate(`/personalitys/${personality_id}`)
+    }
+
+    const openpRroductEditPage = (personality_id) => {
+        navigate(`/personalitys/${personality_id}/edit`)
+    }
+
+    const handleDeletePersonality = async (personality_id) => {
+        dispatch(deletePersonality(personality_id))
+    }
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: '№',
+                accessor: 'id',
+            },
+            {
+                Header: 'Название',
+                accessor: 'name',
+                Cell: ({ value }) => value
+            },
+            {
+                Header: 'Тип лица',
+                accessor: 'type',
+                Cell: ({ value }) => value
+            },
+            {
+                Header: "Действие",
+                accessor: "edit_button",
+                Cell: ({ cell }) => (
+                    <Button color="primary" onClick={() => openpRroductEditPage(cell.row.values.id)}>Редактировать</Button>
+                )
+            },
+            {
+                Header: "Удалить",
+                accessor: "delete_button",
+                Cell: ({ cell }) => (
+                    <Button color="danger" onClick={() => handleDeletePersonality(cell.row.values.id)}>Удалить</Button>
+                )
+            }
+        ],
+        []
+    )
+
+    if (!personalitys.length) {
+        return (
+            <></>
+        )
+    }
+
+    return (
+        <CustomTable columns={columns} data={personalitys} onClick={handleClick} />
+    )
+};
+
+export default PersonalitysTable
